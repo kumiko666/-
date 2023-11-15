b5942ced23c9ff74433b0e46431c210f428b3ad5
kumiko666 <150768628+kumiko666@users.noreply.github.com>
2023-11-15 14:03:59 +0800

    Add files via upload

---
+++ 气泡.cpp
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+int main()
+{
+	int a[10], i, j, k,t=1;
+	for (i = 0; i < 10; i++) {
+		scanf("%d", &a[i]);
+	}
+	printf("\n");
+	for (i = 0; i < 9; i++) {
+		for (j = 9; j > i; j--) {
+			if (a[j] < a[j- 1]) {
+				k = a[j];
+				a[j] = a[j - 1];
+				a[j -1] = k;
+				t = 0;
+			}
+		}
+		if (t == 1)break;
+	}
+	for (i = 0; i < 10; i++) {
+		printf("%4d", a[i]);
+	}
+}
